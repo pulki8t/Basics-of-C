ceb7eb6434a182ecd48fce299f15f9ba425e50e3
pulki8t <99578561+pulki8t@users.noreply.github.com>
2022-04-11 22:56:59 +0530

    Add files via upload

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,38 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
 void main()
 {
-    int size, i,j, arr[100], a;
-    clock_t begin= time(NULL);
-    printf("Enter size of array: ");
-    scanf("%d", &size);
-    for(i=0; i<size; i++)
+    int n, i, j, temp, max, max_index;
+    int arr[100];
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; ++i)
     {
-        arr[i]= rand();
+        arr[i]=rand();
     }
-    printf("Numbers in array are ");
-    for(i=0; i<size; i++)
+    printf("Elements of array are: ");
+    for(i=0; i<n; i++)
     {
         printf("%d ", arr[i]);
     }
-    for(i=0;i<size; i++)
+    max= arr[0];
+    for(i=n-1; i>=0; i--)
     {
-        for(j=i+1; j<size; j++)
+        for(j=0; j<=i; j++)
         {
-            if(arr[i] > arr[j])
+            if (max < arr[j])
             {
-                a= arr[i];
-                arr[i]= arr[j];
-                arr[j]= a;
+                max= arr[j]; 
+                max_index= j;
             }
         }
+        temp= arr[max_index];
+        arr[max_index]= arr[i];
+        arr[i]= temp;
+
     }
-    printf("\nArray when sorted gives the data as ");
-    for(i=0; i<size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    clock_t end = time(NULL);
-    printf("\nTime taken is %f seconds", (double)(end-begin)/CLOCKS_PER_SEC);
+    printf("\n element = ");
+    for(i=0; i<n; i++)
+    printf("%d ", arr[i]);
 }
