c3bbe4d97e6d70d18961cbb8446b7d99d14e61be
pulki8t <99578561+pulki8t@users.noreply.github.com>
2022-07-06 07:03:47 +0530

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,68 +1,54 @@
-#include<stdio.h>
-int rand();
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+  int item;
+  struct node* left;
+  struct node* right;
+};
+
+struct node* create(int data)
+{
+  struct node* x;
+  x= (struct node*)malloc(sizeof(struct node));
+  x->item= data;
+  x->left= NULL;
+  x->right= NULL;
+  return x;
+}
+
+void preorder(struct node* root)
+{
+  if(root!=NULL)
+  {
+    printf("%d ", root->item);
+    preorder(root->left);
+    preorder(root->right);
+  }
+}
+
+void insert(node* new, int data)
+{
+  node* prev= NULL;
+  while(new!=NULL)
+  {
+    prev= new;
+  }
+}
+
 void main()
 {
-    int arr[100];
-    int i, j, n, max, min, sum, average;
-    printf("Enter number of elements of an array: ");
-    scanf("%d", &n);
-    printf("Enter elements of array: ");
-    for(i=0; i<n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    printf("Array is: ");
-    for(i=0; i<n; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    max= arr[0];
-    for(i=0; i<n; i++)
-    {
-        if(max<arr[i])
-        {
-            max= arr[i];
-        }
-    }
-    printf("\nMaximum element in the array is %d", max);
-    min=arr[0];
-    for(i=0; i<n; i++)
-    {
-        if(min > arr[i])
-        {
-            min= arr[i];
-        }
-    }
-    printf("\nMinimum element in the array is %d", min);
-    sum=0;
-    for(i=0; i<n; i++)
-    {
-        sum+=arr[i];
-    }
-    average= sum/n;
-    printf("\nAverage of elements in array is %d", average);
-    printf("\nPair of elements are: ");
-    for(i=0; i<n; i++)
-    {
-        for(j=i+1; j<n; j++)
-        {
-            printf("(%d %d)\n", arr[i], arr[j]);
-        }
-    }
-    printf("\nSum of pairs are: ");
-    for(i=0; i<n; i++)
-    {
-        for(j=i+1; j<n; j++)
-        {
-            printf("(%d)\n", arr[i]+arr[j]);
-        }
-    }
-    printf("\nDifference of pairs are: ");
-    for(i=0; i<n; i++)
-    {
-        for(j=i+1; j<n; j++)
-        {
-            printf("(%d)\n", arr[i]-arr[j]);
-        }
-    }
+  struct node* p = create(15);
+  struct node* pa = create(13);
+  struct node* pb = create(16);
+  struct node* pc = create(11);
+  struct node* pd = create(14);
+
+  p->left= pa;
+  p->right= pb;
+  pa->left= pc;
+  pa->right= pd;
+
+  preorder(p);
 }
