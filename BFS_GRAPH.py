198b9f1c93b6b6f394e43f65225cfee82457c8d8
pulki8t <99578561+pulki8t@users.noreply.github.com>
2022-11-25 14:38:50 +0530

    Update BFS_GRAPH.py

--- BFS_GRAPH.py
+++ BFS_GRAPH.py
@@ -2,14 +2,14 @@ import collections
 
 graph= {'A':['B','C','D'], 'B':['E'], 'C':['D','E'], 'D':[], 'E':[]}
 def bfs(graph, root):
-    visited = set()
-    queue= collections.deque(root)
+    visited = []
+    queue= collections.deque([root])
+
     while queue:
         vertex=queue.popleft()
-        visited.add(vertex)
+        visited.append(vertex)
         for i in graph[vertex]:
-            if i not in visited:
+            if i not in (visited and queue):
                 queue.append(i)
     print(visited)
-
 bfs(graph, 'A')
